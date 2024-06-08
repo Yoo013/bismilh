ff2baa1357073039c66943b1d2d047b50ade6a80
Yoo013 <alfraka744@gmail.com>
2024-06-08 10:54:28 +0700

    first

---
+++ server/index.js
@@ -0,0 +1,30 @@
+import express from 'express'
+import dotenv from 'dotenv'
+import mongoose from 'mongoose'
+import cors from 'cors'
+import cookieParser from 'cookie-parser'
+dotenv.config()
+import{UserRouter} from './routes/user.js'
+
+
+
+const app = express()
+
+
+
+app.use(express.json())
+app.use(cors({
+    origin: ["http://localhost:5173"],
+    credentials: true
+}))
+app.use(cookieParser())
+app.use('/auth', UserRouter)
+
+
+
+mongoose.connect('mongodb+srv://kelas:L71noEVRLPMX9Rth@kelas.eei6kwp.mongodb.net/authentication')
+
+
+app.listen(process.env.PORT, () => {
+    console.log("Server is Running")
+})
