9ff218e5dbc72c46e3a7af85bc643431c9d7410f
Yoo013 <alfraka744@gmail.com>
2024-06-08 11:04:18 +0700

    bis

--- server/index.js
+++ server/index.js
@@ -14,7 +14,7 @@ const app = express()
 
 app.use(express.json())
 app.use(cors({
-    origin: ["http://localhost:5173"],
+    origin: ["https://bismilh.vercel.app/"],
     credentials: true
 }))
 app.use(cookieParser())
