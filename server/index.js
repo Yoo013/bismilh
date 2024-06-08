0f16fca83b8cf64bc41330174043cb23476b90c2
Yoo013 <alfraka744@gmail.com>
2024-06-08 12:38:16 +0700

    Update index.js

--- server/index.js
+++ server/index.js
@@ -14,7 +14,7 @@ const app = express()
 
 app.use(express.json())
 app.use(cors({
-    origin: ["https://bismilh-ffxi.vercel.app"],
+    origin: ["https://bismilh.vercel.app/"],
     methods: ["POST", "GET"],
     credentials: true
 }))
